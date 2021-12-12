92186a3a60c93a422ad27b42c3dd4ef2ab5ffe71
mohansingh <singhmohan333@gmail.com>
2021-12-12 12:35:51 +0530

    d

---
+++ old/index - Copy.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Jagriti Institute of Higher Education</title>
+<link href="style/style_sheet.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<table width="1000" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
+  <tr>
+    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="93" height="79" align="right" background="image/top_bg.gif"><img src="image/logo.gif" alt="Jagriti Institute of Higher Education" title="Jagriti Institute of Higher Education" /></td>
+        <td width="569" background="image/top_bg.gif"><img src="image/logo_1.gif" alt="Jagriti Institute of Higher Education" title="Jagriti Institute of Higher Education"  width="461" height="24" /></td>
+        <td width="36" background="image/top_bg.gif"><a href="index.php"><img src="image/home.gif" alt="Home" width="11" height="9" border="0" /></a></td>
+        <td width="36" background="image/top_bg.gif"><a href="contact_us.php"><img src="image/contact_us.gif" alt="Contact Us" width="10" height="7" border="0" /></a></td>
+        <td width="45" background="image/top_bg.gif"><a href="sitemap.php"><img src="image/sitemap.gif" alt="Site Map" width="11" height="11" border="0" /></a></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td height="186"><img src="image/header.gif" width="1000" height="500" /></td>
+  </tr>
+  <tr>
+    <td background="image/menu_bg.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="39" height="38" align="center" valign="top" style="padding-top:10px;"><a href="index.php" class="textlink" >Home</a></td>
+        <td width="109" align="center" valign="top" style="padding-top:10px;"><a href="college_details.php" class="textlink">College Details</a></td>
+        <td width="109" align="center" valign="top" style="padding-top:10px;"><a href="management.php" class="textlink">About Management</a></td>
+        <td width="61" align="center" valign="top" style="padding-top:10px;"><a href="courses.php" class="textlink">Courses</a></td>
+        <td width="64" align="center"valign="top" style="padding-top:10px;"><a href="facilities.php" class="textlink">Facilities</a></td>
+        <td width="47" align="center"valign="top" style="padding-top:10px;"><a href="faculty.php" class="textlink">Faculty</a></td>
+        <td width="125" align="center"valign="top" style="padding-top:10px;"><a href="chairman.php" class="textlink">Chairman Message</a></td>
+        <td width="104" align="center"valign="top" style="padding-top:10px;"><a href="principal.php" class="textlink">Principal Message</a></td>
+        <td width="104" align="center"valign="top" style="padding-top:10px;"><a href="sarreport.php" class="textlink">SAR Report</a></td>
+        <td width="104" align="center"valign="top" style="padding-top:10px;"><a href="gallery.php" class="textlink">Gallery</a></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+<table width="1000" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
+  <tr>
+    <td colspan="3"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td colspan="2">&nbsp;</td>
+      </tr>
+      <tr>
+        <td width="192" rowspan="4" align="center" valign="top"><img src="image/college_img.gif" alt="College" title="College" width="165" height="178" /></td>
+        <td><img src="image/welcome.gif" alt="Welcome to Jagriti Institute of Higher Eduction" title="Welcome to Jagriti Institute of Higher Eduction" width="575" height="34" /></td>
+      </tr>
+      <tr>
+        <td valign="top" class="middtext"><p>Jagriti Institute of Higher Education is run by Jagriti Shiksha  Samiti Mohna. The college is situated in Mohna, Ballabgarh, Distt-Faridabad  Haryana. We are running a course for Bachelor of Education and Diploma in Education. Our aim is to  provide opportunities for all students to achieve their full potential and this  can be done through collective efforts of faculty, staff members and students.  The college provides all those facilities which are essential for making would  be teachers.</p></td>
+      </tr>
+      <tr></tr>
+      <tr>
+        <td valign="top" class="middtext"><p>The course Bachelor of Education (B.Ed.) is meant for preparing  pillars of nation. &nbsp;The duration of  course for Bachelor of Education is one year and the minimum eligibility is  graduation with 50%* or above marks (prescribed by the state) and the course Diploma in Education (D.Ed.) </p></td>
+      </tr>
+      <tr>
+        <td colspan="2">&nbsp;</td>
+      </tr>
+      <tr>
+        <td colspan="2" background="image/midd_dot.gif" height="2"></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td colspan="3">&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="260" align="center" valign="top"><table width="90%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td height="10" valign="top"><img src="image/box_left_top.gif" width="8" height="10" /></td>
+        <td height="5" background="image/box_top_midd.gif" style="background-repeat:repeat-x;"></td>
+        <td height="10" valign="top"><img src="image/box_right_top.gif" width="8" height="10" /></td>
+      </tr>
+      <tr>
+        <td width="8" valign="top" background="image/box_left_green.gif">&nbsp;</td>
+        <td height="62" align="center" bgcolor="#D5DB55"><h2>Vision</h2></td>
+        <td width="8" valign="top" background="image/box_right_green.gif">&nbsp;</td>
+      </tr>
+      <tr>
+        <td background="image/box_left_wight.gif">&nbsp;</td>
+        <td align="left" valign="top" bgcolor="#FAFAFA"><p>We are eager to  impart a sound formation in all fields such as social, moral, economic,  academic, physical, emotional through <strong>'Teach One &ndash; Each One'</strong>. </p></td>
+        <td background="image/box_right_wight.gif">&nbsp;</td>
+      </tr>
+      <tr>
+        <td height="8" valign="top"><img src="image/box_bottom_left.gif" width="8" height="8" /></td>
+        <td height="8" background="image/box_bottom_midd.gif"></td>
+        <td height="8" valign="top"><img src="image/box_bottom_right.gif" width="8" height="8" /></td>
+      </tr>
+    </table></td>
+    <td width="260" align="center" valign="top"><table width="90%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td height="10" valign="top"><img src="image/box_left_top.gif" width="8" height="10" /></td>
+        <td height="5" background="image/box_top_midd.gif" style="background-repeat:repeat-x;"></td>
+        <td height="10" valign="top"><img src="image/box_right_top.gif" width="8" height="10" /></td>
+      </tr>
+      <tr>
+        <td width="8" valign="top" background="image/box_left_green.gif">&nbsp;</td>
+        <td height="62" align="center" bgcolor="#D5DB55"><h2>Mission</h2></td>
+        <td width="8" valign="top" background="image/box_right_green.gif">&nbsp;</td>
+      </tr>
+      <tr>
+        <td background="image/box_left_wight.gif">&nbsp;</td>
+        <td align="left" valign="top" bgcolor="#FAFAFA"><p>Our society is  eager to in thrust the skills with competencies, intellectual leadership,  scholarly contributions, design of courses, experimental learning, solving  educational problems among the pupil-teachers to prepare them to face  challenges in a rapidly changing world. </p></td>
+        <td background="image/box_right_wight.gif">&nbsp;</td>
+      </tr>
+      <tr>
+        <td height="8" valign="top"><img src="image/box_bottom_left.gif" width="8" height="8" /></td>
+        <td height="8" background="image/box_bottom_midd.gif"></td>
+        <td height="8" valign="top"><img src="image/box_bottom_right.gif" width="8" height="8" /></td>
+      </tr>
+    </table></td>
+    <td width="260" align="center" valign="top"><table width="90%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td height="10" valign="top"><img src="image/box_left_top.gif" width="8" height="10" /></td>
+        <td height="5" background="image/box_top_midd.gif" style="background-repeat:repeat-x;"></td>
+        <td height="10" valign="top"><img src="image/box_right_top.gif" width="8" height="10" /></td>
+      </tr>
+      <tr>
+        <td width="8" valign="top" background="image/box_left_green.gif">&nbsp;</td>
+        <td height="62" align="center" bgcolor="#D5DB55"><h2>Value</h2></td>
+        <td width="8" valign="top" background="image/box_right_green.gif">&nbsp;</td>
+      </tr>
+      <tr>
+        <td background="image/box_left_wight.gif">&nbsp;</td>
+        <td align="left" valign="top" bgcolor="#FAFAFA"><p>All our activities  and programmes are to increase social, economic, religious &amp; physical  equality. Special focus has been given to protect, conservate the nature and  environment through our institution.&nbsp;&nbsp;&nbsp;&nbsp; </p></td>
+        <td background="image/box_right_wight.gif">&nbsp;</td>
+      </tr>
+      <tr>
+        <td height="8" valign="top"><img src="image/box_bottom_left.gif" width="8" height="8" /></td>
+        <td height="8" background="image/box_bottom_midd.gif"></td>
+        <td height="8" valign="top"><img src="image/box_bottom_right.gif" width="8" height="8" /></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td colspan="3">&nbsp;</td>
+  </tr>
+</table>
+<table width="1000" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
+  <tr>
+    <td height="76" align="center" valign="top" background="image/footer.gif"><table width="80%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="center"><a href="index.php" class="textlink1">Home</a></td>
+            <td width="1" align="center">|</td>
+            <td width="110" align="center"><a href="college_details.php" class="textlink1">College Details</a></td>
+            <td width="1" align="center">|</td>
+            <td width="70" align="center"><a href="courses.php" class="textlink1">Courses</a> </td>
+            <td width="1" align="center">|</td>
+            <td width="70" align="center"><a href="facilities.php" class="textlink1">Facilities</a></td>
+            <td width="1" align="center">|</td>
+            <td width="60" align="center"><a href="faculty.htmlk" class="textlink1">Faculty</a></td>
+           <td width="1" align="center">|</td>
+            <td width="60" align="center"><a href="latest.php" class="textlink1">Latest</a> </td>
+            <td width="1" align="center">|</td>
+            <td width="90" align="center"><a href="help_desk.php" class="textlink1">Help Desk</a></td>
+            <td width="1" align="center">|</td>
+            <td width="70" align="center"><a href="sitemap.php" class="textlink1">Site Map</a></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr></tr>
+      <tr>
+        <td height="20" align="center">Copyright &copy; 2007-2008 Jagriti Insritute of Higher Education</td>
+      </tr>
+      <tr>
+        <td align="center" class="design">Design &amp; Maintained By <a href="http://www.kzinesolutions.com" class="Kzinetext">Kzine Solutions</a></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+</body>
+</html>
